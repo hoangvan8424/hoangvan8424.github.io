245cc72f32795709208b626a8dd011a7f660e3df
Hoang Van Thai <46862606+hoangvan8424@users.noreply.github.com>
2020-04-02 21:16:54 +0700

    tạo bảng transactions (đơn hàng)

--- database/migrations/2020_04_02_205622_create_transactions_table.php
+++ database/migrations/2020_04_02_205622_create_transactions_table.php
@@ -15,7 +15,16 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('tr_user_id')->unsigned();
+            $table->string('tr_user_name');
+            $table->string('tr_user_email')->nullable();
+            $table->string('tr_user_phone');
+            $table->string('tr_address');
+            $table->integer('tr_total')->default(0);
+            $table->text('tr_note')->nullable();
+            $table->tinyInteger('tr_status')->default(0)->index();
             $table->timestamps();
+            $table->foreign('tr_user_id')->references('id')->on('users');
         });
     }
 
