5c509f0d6107dc332e455a7b8db3f4a9c4561d49
Hoang Van Thai <46862606+hoangvan8424@users.noreply.github.com>
2020-04-04 23:23:52 +0700

    tích hợp trình soạn thảo ckeditor vào website

--- resources/views/admin/transaction/index.blade.php
+++ resources/views/admin/transaction/index.blade.php
@@ -52,7 +52,7 @@
 
                             </td>
                             <td>
-                                <a data-toggle="modal" data-target="#myModal" data-id="{{ $transactions->id }}" data-href="{{ route('admin.get.view.order', $transactions->id) }}" class="quick-view-order">
+                                <a data-toggle="modal" data-target="#myModal" data-id="{{ $transactions->id }}" data-href="{{ route('admin.get.view.order', $transactions->id) }}" class="quick-view-order" type="button">
                                     <i class="far fa-eye"></i>
                                 </a>
                             </td>
@@ -71,14 +71,11 @@
                 <!-- Modal Header -->
                 <div class="modal-header">
                     <h4 class="modal-title transaction-id">Modal Heading</h4>
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
-
                 <!-- Modal body -->
                 <div class="modal-body" id="modal-content">
 
                 </div>
-
                 <!-- Modal footer -->
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Đóng</button>
