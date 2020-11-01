56617b3480b489f841cf5e1f9c9b1a4639329349
Hoang Van <hoangvan181198@gmail.com>
2020-11-01 23:36:59 +0700

    edit table products

---
+++ database/migrations/2020_11_01_161147_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('branch_id')->default(0);
+            $table->text('name')->nullable();
+            $table->float("price")->default(0);
+            $table->integer('quantity');
+            $table->text('note')->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
