18a6c0a6cb5fd4109150bf9024f896e0b7cfce74
Hoang Van <hoangvan181198@gmail.com>
2020-11-01 22:21:46 +0700

    create departments table (phòng ban)

---
+++ database/migrations/2020_11_01_151852_create_departments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->id();
+            $table->text('name')->nullable();
+            $table->integer('branch_id');
+            $table->text('note')->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departments');
+    }
+}
