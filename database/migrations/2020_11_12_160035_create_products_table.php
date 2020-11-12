2e1d9317e53000c03415b3eec5d0ff1a402f135c
Hoang Van <hoangvan181198@gmail.com>
2020-11-12 23:13:57 +0700

    create category and product table

---
+++ database/migrations/2020_11_12_160035_create_products_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('category_id');
+            $table->string('name');
+            $table->string('slug')->nullable();
+            $table->integer('price')->default(0);
+            $table->integer('sale')->default(0);
+            $table->string('image_1')->nullable();
+            $table->string('image_2')->nullable();
+            $table->string('image_3')->nullable();
+            $table->string('image_4')->nullable();
+            $table->string('image_5')->nullable();
+            $table->longText('description')->nullable();
+            $table->longText('information')->nullable();
+            $table->integer('total_number')->default(0);
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
