d84fbf32b2d228542f2c3cfed777be412c679fc0
Hoang Van <hoangvan181198@gmail.com>
2020-11-12 23:16:58 +0700

    create model for product and category model

---
+++ app/Model/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'category_id',
+        'name',
+        'slug',
+        'price',
+        'sale',
+        'image_1',
+        'image_2',
+        'image_3',
+        'image_4',
+        'image_5',
+        'description',
+        'information',
+        'total_number',
+        'active',
+
+    ];
+}
