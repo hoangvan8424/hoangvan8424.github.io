8729665df6c222a7662f2866041310000f38dec8
Hoang Van <hoangvan181198@gmail.com>
2020-11-26 17:23:35 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,32 +5,52 @@ $(document).ready(function() {
         $(".navbar-burger").toggleClass("is-active");
         $('.navbar-menu').toggleClass('is-active');
 
-        $('.menu-header-item a').removeClass('active');
+
         $(this).addClass('active');
 
+        $('.menu-header-item a.active').removeClass('active');
         let section = $(this).attr('href');
         let position = $(section).offset().top - 100;
-        $('html, body').animate({scrollTop: position},600);
+        $('html, body').stop().animate(
+            {scrollTop: position},
+            600, function () {
+                location.hash = $(section);
+            });
+        return false;
+
+    });
+
+    $('.cart a').click(function(event) {
+        event.preventDefault();
+        let section = $(this).attr('href');
+        let position = $(section).offset().top - 100;
+        $('html, body').stop().animate(
+            {scrollTop: position},
+            600, function () {
+                location.hash = $(section);
+            });
+        return false;
+
     });
 
 
-    // var header = $('#header');
-    // var origOffsetY = header.offset().top + 100;
-    // var status = true;
-    // $(window).scroll(function () {
-    //     if($(window).scrollTop() >= origOffsetY) {
-    //         if(status === true) {
-    //             header.addClass('header-fix');
-    //             status = false;
-    //         }
-    //     }
-    //     else {
-    //         if(status === false) {
-    //             header.removeClass('header-fix');
-    //             status = true;
-    //         }
-    //     }
-    // });
+    var header = $('#header');
+    var origOffsetY = header.offset().top + 20;
+    var status = true;
+    $(window).scroll(function () {
+        if($(window).scrollTop() >= origOffsetY) {
+            if(status === true) {
+                header.addClass('header-fix');
+                status = false;
+            }
+        }
+        else {
+            if(status === false) {
+                header.removeClass('header-fix');
+                status = true;
+            }
+        }
+    });
 
     $('#back-to-top').click(function (event) {
         event.preventDefault();
