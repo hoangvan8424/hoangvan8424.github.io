7a47f6fa790f96416e314a48e504605474ff6578
Hoang Van <hoangvan181198@gmail.com>
2020-11-26 12:56:48 +0700

    update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,13 +46,13 @@ $(document).ready(function() {
     });
 });
 
-// $(window).scroll(function() {
-//     var scrollDistance = $(window).scrollTop();
-//
-//     $('.page-section').each(function(i) {
-//         if ($(this).position().top == scrollDistance) {
-//             $('.menu-header-item a.active').removeClass('active');
-//             $('.menu-header-item a').eq(i).addClass('active');
-//         }
-//     });
-// }).scroll();
+$(window).scroll(function() {
+    var scrollDistance = $(window).scrollTop();
+
+    $('.page-section').each(function(i) {
+        if (($(this).position().top) - 110 <= scrollDistance) {
+            $('.menu-header-item a.active').removeClass('active');
+            $('.menu-header-item a').eq(i).addClass('active');
+        }
+    });
+}).scroll();
